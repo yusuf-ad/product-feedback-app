9966e1bcef72566110b15dfeda4e8456ee629bcd
yusuf <yusufgad24@gmail.com>
2023-12-16 13:17:29 +0300

    vercel

--- api/server.js
+++ api/server.js
@@ -10,10 +10,6 @@ mongoose
   .connect(DB)
   .then(() => {
     console.log("App connected to database");
-
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
-    });
   })
   .catch((error) => {
     console.log(error);
