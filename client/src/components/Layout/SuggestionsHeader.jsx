b1b7b0ebf39b5e70bca4e4ef95d3c10847573925
yusuf <yusufgad24@gmail.com>
2023-12-16 18:24:12 +0300

    responsive-phone

--- client/src/components/Layout/SuggestionsHeader.jsx
+++ client/src/components/Layout/SuggestionsHeader.jsx
@@ -3,15 +3,16 @@ import SortButton from "../UI/SortButton";
 
 export function SuggestionsHeader({ numFeedbacks }) {
   return (
-    <header className="px-6 py-4 bg-grey-darker rounded-xl flex gap-8  ">
-      <div className="flex items-center gap-4">
-        <img src="./assets/suggestions/icon-suggestions.svg" alt="bulb" />
-        <p className="font-bold mt-1 text-white">{numFeedbacks} Suggestions</p>
+    <header className="px-6 py-4 bg-grey-darker rounded-xl flex flex-col  gap-8  ">
+      <div className="md:block flex justify-between  ">
+        <div className="flex items-center gap-4">
+          <img src="./assets/suggestions/icon-suggestions.svg" alt="bulb" />
+          <p className="font-bold  text-white">{numFeedbacks} Suggestions</p>
+        </div>
+        <SortButton />
       </div>
 
-      <SortButton />
-
-      <Link to="/feedback/add" className="ml-auto">
+      <Link to="/feedback/add" className="md:ml-auto self-center">
         <button className="btn  bg-purple-default   hover:bg-purple-hover">
           <i className="text-xs  fa-solid fa-plus"></i>
           <span className="ml-2">Add Feedback</span>
