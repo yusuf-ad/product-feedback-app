bab47adcd3b7ba1306aa349d76ef527be8d0d2bc
yusuf <yusufgad24@gmail.com>
2023-12-16 13:19:40 +0300

    vercel

--- api/app.js
+++ api/app.js
@@ -20,9 +20,9 @@ app.use(express.json());
 // ! WHERE OUR ROUTER MOUNTS
 // ! They are simply middleware functions that only apply for a certain URL.
 
-app.get("/", (req, res) => res.json("hello, from the server"));
+// app.get("/", (req, res) => res.json("hello, from the server"));
 
-app.use("/api/v1/feedbacks", feedbackRouter);
+app.use("/", feedbackRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/replies", replyRouter);
 
